c4dbb97f770aaddbdf5e64f5c3bc22d2c2dc69af
Fantin <fantin@device-193.home>
2026-04-26 16:06:39 +0200

    feat: add /admin routes to router

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
 import Dashboard from './pages/Dashboard'
 import OrderWorkspace from './pages/OrderWorkspace'
+import Admin from './pages/admin/Admin'
+import AdminModelNew from './pages/admin/AdminModelNew'
+import AdminModelEdit from './pages/admin/AdminModelEdit'
 import ProtectedRoute from './components/ui/protected-route'
+import AdminRoute from './components/ui/admin-route'
 import './index.css'
 
 const router = createBrowserRouter([
@@ -16,6 +20,18 @@ const router = createBrowserRouter([
   {
     path: '/dashboard/:orderId',
     element: <ProtectedRoute><OrderWorkspace /></ProtectedRoute>,
+  },
+  {
+    path: '/admin',
+    element: <AdminRoute><Admin /></AdminRoute>,
+  },
+  {
+    path: '/admin/models/new',
+    element: <AdminRoute><AdminModelNew /></AdminRoute>,
+  },
+  {
+    path: '/admin/models/:id',
+    element: <AdminRoute><AdminModelEdit /></AdminRoute>,
   },
 ])
 
